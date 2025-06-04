54a511ced171d306f984dbf472934bfd7061211a
TimofeyMaximov <135747651+TimofeyMaximov@users.noreply.github.com>
2025-06-04 11:20:34 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,33 +11,66 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 let waitingPlayer = null;
 
+function setupGame(p1, p2) {
+  p1.enemy = p2;
+  p2.enemy = p1;
+  p1.board = {};
+  p2.board = {};
+  p1.ready = false;
+  p2.ready = false;
+  p1.isTurn = true;
+  p2.isTurn = false;
+
+  p1.send(JSON.stringify({ type: 'waitForSetup' }));
+  p2.send(JSON.stringify({ type: 'waitForSetup' }));
+}
+
 wss.on('connection', (ws) => {
   if (waitingPlayer === null) {
     waitingPlayer = ws;
-    ws.send(JSON.stringify({ type: 'wait' }));
+    ws.send(JSON.stringify({ type: 'waiting' }));
   } else {
-    const player1 = waitingPlayer;
-    const player2 = ws;
+    const p1 = waitingPlayer;
+    const p2 = ws;
     waitingPlayer = null;
-
-    player1.opponent = player2;
-    player2.opponent = player1;
-
-    player1.send(JSON.stringify({ type: 'start', yourTurn: true }));
-    player2.send(JSON.stringify({ type: 'start', yourTurn: false }));
-
-    [player1, player2].forEach(player => {
-      player.on('message', msg => {
-        const data = JSON.parse(msg);
-        if (player.opponent && player.opponent.readyState === WebSocket.OPEN) {
-          player.opponent.send(JSON.stringify(data));
-        }
-      });
-    });
+    setupGame(p1, p2);
   }
 
+  ws.on('message', (msg) => {
+    const data = JSON.parse(msg);
+    const enemy = ws.enemy;
+
+    if (data.type === 'setup') {
+      ws.board = data.board;
+      ws.ready = true;
+      if (enemy && enemy.ready) {
+        ws.send(JSON.stringify({ type: 'start', yourTurn: ws.isTurn }));
+        enemy.send(JSON.stringify({ type: 'start', yourTurn: enemy.isTurn }));
+      }
+    }
+
+    if (data.type === 'fire' && ws.isTurn) {
+      const key = `${data.x},${data.y}`;
+      const hit = enemy.board[key] === true;
+      if (hit) enemy.board[key] = false;
+
+      ws.send(JSON.stringify({ type: 'result', x: data.x, y: data.y, hit }));
+      enemy.send(JSON.stringify({ type: 'incoming', x: data.x, y: data.y, hit }));
+
+      ws.isTurn = false;
+      enemy.isTurn = true;
+
+      enemy.send(JSON.stringify({ type: 'yourTurn' }));
+    }
+  });
+
   ws.on('close', () => {
-    if (ws.opponent) ws.opponent.send(JSON.stringify({ type: 'disconnect' }));
+    if (ws.enemy) {
+      ws.enemy.send(JSON.stringify({ type: 'disconnect' }));
+    }
+    if (waitingPlayer === ws) {
+      waitingPlayer = null;
+    }
   });
 });
 
